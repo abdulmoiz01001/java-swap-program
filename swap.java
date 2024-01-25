d02e9515a305be5dc4c0adc505fd195fdb488370
abdulmoiz01001 <profession.ai01001@hotmail.com>
2024-01-25 13:03:16 +0500

    Swap numbers in JAVA by 2 and 3 variables

---
+++ swap.java
@@ -0,0 +1,31 @@
+
+// Swap 2 number by 3 variables 
+/*
+public class swap {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int temp;
+        System.out.println("Before swapping: a, b = "+a+", "+  b);
+        temp = a;
+        a = b;
+        b= temp;
+        System.out.println("After swapping: a, b = "+a+", "+  b);
+    }
+} */
+
+
+// Swap 2 number by 2 variables
+/*
+public class swap {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swapping: a, b = "+a+", "+  b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After swapping: a, b = "+a+", "+  b);
+    }
+}  */
+
